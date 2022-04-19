e56f8044af89bdb36a1798dd2a6342a938a4f6d5
Chirag Simkhada <chiragsimkhada@gmail.com>
2022-04-19 07:51:48 +0545

    Fixing video call 'add user' issue

--- public/src/pages/VideoCall.jsx
+++ public/src/pages/VideoCall.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { Component, useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import { useNavigate, useParams } from "react-router-dom";
 import { io } from "socket.io-client";
@@ -12,6 +12,11 @@ export default function VideoCall() {
   const roomID = params.roomId;
 
   var currentUser;
+  const myPeer = new Peer(undefined, {
+    host: "/",
+    port: "3001",
+  });
+
   useEffect(() => {
     if (!localStorage.getItem("user")) {
       navigate("/login");
@@ -25,24 +30,19 @@ export default function VideoCall() {
   useEffect(() => {
     if (currentUser) {
       socket.current = io(host);
-      console.log("Loaded");
     }
   }, [currentUser]);
 
-  // useEffect(() => {
-  //   socket.current.emit("join-room", {
-  //     roomId: roomID,
-  //     userId: currentUser._id,
-  //   });
-  //   socket.current.on("user-connected", (userId) => {
-  //     console.log("User Connected", userId);
-  //   });
-  // }, []);
+  useEffect(() => {
+    socket.current.emit("join-room", {
+      roomId: roomID,
+      userId: currentUser._id,
+    });
 
-  const myPeer = new Peer(undefined, {
-    host: "/",
-    port: "3001",
-  });
+    socket.current.on("user-connected", (userId) => {
+      console.log("User Connected", userId);
+    });
+  }, []);
 
   myPeer.on("open", (id) => {
     socket.current.emit("join-room", {
@@ -51,7 +51,38 @@ export default function VideoCall() {
     });
   });
 
-  return <div id="video_grid">{roomID}</div>;
+  // let videoGrid;
+
+  // function addVideoStream(video, stream) {
+  //   video.srcObject = stream;
+  //   video.addEventListener("loadedmetadata", () => {
+  //     video.play();
+  //   });
+  //   videoGrid.append(video);
+  // }
+  // function connectToNewUser(userId, stream) {
+  //   const call = myPeer.call(userId, stream);
+  //   console.log("Is ans?");
+  //   const video = document.createElement("video");
+  //   call.on("stream", (userVideoStream) => {
+  //     console.log(userVideoStream);
+  //     addVideoStream(video, userVideoStream);
+  //   });
+  //   call.on("close", () => {
+  //     video.remove();
+  //   });
+
+  //   // peers[userId] = call;
+  // }
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "../scripts/videoChat.js";
+    script.async = true;
+    document.body.appendChild(script);
+  }, []);
+
+  return <div id="video-grid"></div>;
 }
 
 const video_grid = styled.div`
