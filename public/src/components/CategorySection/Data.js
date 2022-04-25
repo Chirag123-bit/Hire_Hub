4d8cb2a9c4955f4fa2d2abbee3be940d682de03f
Chirag Simkhada <chiragsimkhada@gmail.com>
2022-04-25 20:05:35 +0545

    Completed frontend of category section

---
+++ public/src/components/CategorySection/Data.js
@@ -0,0 +1,18 @@
+export const catObjOne = {
+  Sector: "Technology",
+  Job_Title: "Backend Developer",
+  no_of_jobs: "5",
+  white: false,
+};
+export const catObjTwo = {
+  Sector: "Real Estate",
+  Job_Title: "Broker Agent",
+  no_of_jobs: "10",
+  white: true,
+};
+export const catObjThree = {
+  Sector: "Education",
+  Job_Title: "Professors",
+  no_of_jobs: "8",
+  white: false,
+};
--- public/src/components/SeekerCategory.js
+++ public/src/components/SeekerCategory.js
@@ -1,9 +1,14 @@
 import React from "react";
 import CategoryDescription from "./CategoryLoadSection";
+import Categories from "./CategorySection";
+import { catObjOne, catObjThree, catObjTwo } from "./CategorySection/Data";
 const SeekerHome = () => {
   return (
     <div id="category">
       <CategoryDescription />
+      <Categories {...catObjOne} />
+      <Categories {...catObjTwo} />
+      <Categories {...catObjThree} />
     </div>
   );
 };
